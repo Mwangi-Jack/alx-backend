07ed613c5d5d287be72619255a467b7e3fc09103
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-22 11:58:52 +0300

    deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE, encoding='utf-8') as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+            """
+            This method takes in two integer arguments 'index' with default value None
+            and 'page_size' with default value 10. The method then returns a ke-value
+            pair dictionary
+            """
+
+            assert index is not None and 0 <= index < len(self.dataset())
+            indexed_data = self.indexed_dataset()
+            data = []
+            current_index = index
+
+            while len(data) < page_size and current_index < len(indexed_data):
+                if current_index in indexed_data:
+                    data.append(indexed_data[current_index])
+
+                current_index += 1
+
+            next_index = current_index
+
+            return {'index': index, 'next_index': next_index,
+                    'page_size': page_size, 'data': data}
+
