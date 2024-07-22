e118c84019e9a8a0d3fb3f2f9a40c1054c15d1fe
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-22 10:17:54 +0300

    hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -17,9 +17,9 @@ def index_range(page: int, page_size: int) -> Tuple:
     if page - 1 == 0:
         return (0, page_size)
 
-    start = (page - 1 ) * page_size
+    start = (page - 1) * page_size
     end = page * page_size
-    return (start , end)
+    return (start, end)
 
 
 class Server:
@@ -54,20 +54,21 @@ class Server:
         self.dataset()
         indexes = index_range(page, page_size)
 
-        return [data for data in self.__dataset[indexes[0]: indexes[1]] ]
+        return [data for data in self.__dataset[indexes[0]: indexes[1]]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """
-        This method takes in integers 'page' and 'page_size' and returns
-        a dictionary
+        This method takes two integer arguments 'page' and 'page_size'
+        with default values 1 and 10 and returns a dictionary containing
+        pagination information.
         """
 
         data = self.get_page(page, page_size)
-        return {
-			'page_size': len(data),
-			'page': page,
-			'data': data,
-			'next_page': page + 1,
-			'prev_page': page - 1,
-			'total_pages': len(self.__dataset)
-		}
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {'page_size': page_size, 'page': page, 'data': data,
+                'next_page': next_page, 'prev_page': prev_page,
+                'total_pages': total_pages}
