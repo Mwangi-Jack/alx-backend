eff9acb70a808858ad68d2a4b0941ee0776cdcc9
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-23 14:49:41 +0300

    FIFO Caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""FIFO Caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """This class inherits from BaseCaching"""
+
+    def __init__(self):
+        """
+        This method initializes the class with an inherited
+        super class 'BaseCaching'
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        This method adds a key-value pair to the cache_data
+        """
+
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+                first_key = list(self.cache_data.keys())[0]
+
+                print(f'DISCARD {first_key}')
+                del self.cache_data[first_key]
+
+    def get(self, key):
+        """
+        This method takes in a 'key' argument and returns
+        a the value associated by the provided 'key'
+        """
+        if not key or not self.cache_data.get(key):
+            return None
+
+        return self.cache_data.get(key)
+
+
+
