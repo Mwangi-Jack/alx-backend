c8e7ec1a6e08bbfb628c927f357a178d29cae109
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-24 14:48:45 +0300

    Last Recently Used (LRU) Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -22,11 +22,8 @@ class LRUCache(BaseCaching):
         This method adds a key-value pair to the cache_data
         """
         if key and item:
-            if key in self.cache_data:
-                self.cache_data.move_to_end(key)
-
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
                 first_key = next(iter(self.cache_data))
                 print(f'DISCARD: {first_key}')
                 del self.cache_data[first_key]
@@ -39,4 +36,5 @@ class LRUCache(BaseCaching):
         if not key or not self.cache_data.get(key):
             return None
 
+        self.cache_data.move_to_end(key)
         return self.cache_data.get(key)
