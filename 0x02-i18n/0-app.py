8c9c844661310ad0e8dc37dddf132e0b0643675f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-30 12:27:55 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """This file defines the routes"""
 
+from typing import Callable
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> Callable:
     """this method gets the root page of the app"""
     return render_template('0-index.html',
                            title='Welcome to Holberton', header='Hello world')
