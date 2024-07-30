94848edbf8a3db194365d82e5cf5e33b5aa4e582
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-30 12:17:14 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,9 +6,11 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 def get_locale():
     """gets the language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 babel = Babel(locale_selector=get_locale)
 
@@ -16,4 +18,5 @@ babel = Babel(locale_selector=get_locale)
 @app.route('/')
 def index():
     """this method gets the root page of the app"""
-    return render_template('0-index.html', title='Welcome to Holberton', header='Hello world')
+    return render_template('0-index.html',
+                           title='Welcome to Holberton', header='Hello world')
