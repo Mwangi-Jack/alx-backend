d4b6292a493db26a0acf605530ef36340132e80e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-30 12:17:45 +0300

    Basic flask app

--- 0x02-i18n/config.py
+++ 0x02-i18n/config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Config file"""
 
+
 class Config:
     """This class defines the configuration of the app"""
 
-    LANGUAGES = ['en','fr', 'es', 'sw']
+    LANGUAGES = ['en', 'fr', 'es', 'sw']
     TIMEZONE = 'UTC'
